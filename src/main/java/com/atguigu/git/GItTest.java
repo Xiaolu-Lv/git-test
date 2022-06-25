71bce1bcdf3c64ab47ae5a12c119f3da40a49a9a
Xiaolu Lv <lvxiaolu_1997@163.com>
2022-06-25 16:25:05 +0800

    req1 commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -8,5 +8,6 @@ public class GItTest {
         System.out.println("hello git!");
         System.out.println("hello git!22");
         System.out.println("hello git!33");
+        System.out.println("hello git!44");
     }
 }
