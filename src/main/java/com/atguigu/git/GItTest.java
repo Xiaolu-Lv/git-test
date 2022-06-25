c8d9997acb26ff1300fae79ee6c6718f8877020b
Xiaolu Lv <lvxiaolu_1997@163.com>
2022-06-25 16:16:59 +0800

    gitee commit

---
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author layne
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!22");
+    }
+}
