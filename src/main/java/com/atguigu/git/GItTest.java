880ec08b7c4a0037f146be533461adedb196831f
Xiaolu Lv <lvxiaolu_1997@163.com>
2022-06-25 16:17:56 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -7,5 +7,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!22");
+        System.out.println("hello git!33");
     }
 }
